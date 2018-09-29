4faed6fec190ed2adf26f4e11a9bcd35511f4419
Adam <datriphopper@gmail.com>
2018-09-29 18:52:33 -0500

    Filled in Card and CardAdapter classes

--- app/src/main/java/com/apps/adam/inventory/Card.java
+++ app/src/main/java/com/apps/adam/inventory/Card.java
@@ -1,4 +1,38 @@
 package com.apps.adam.inventory;
 
-public class Card {
+
+
+public class Card  {
+
+    private String Title;
+    private Double Price;
+    private Integer Quantity;
+
+    //Constructor method
+    public Card(String title, Double price, int quantity) {
+        Title = title;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    //Getter methods to access private variables
+    public String getTitle() {
+        return Title;
+    }
+
+    public Double getPrice() {
+        return Price;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/apps/adam/inventory/CardAdapter.java
+++ app/src/main/java/com/apps/adam/inventory/CardAdapter.java
@@ -1,4 +1,62 @@
 package com.apps.adam.inventory;
 
-public class CardAdapter {
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardHolder> {
+    private ArrayList<Card> cards;
+    private Context context;
+    private LayoutInflater inflater;
+
+    public CardAdapter(Context context, ArrayList<Card> cards) {
+        this.inflater = LayoutInflater.from(context);
+        this.cards = cards;
+    }
+
+
+
+    @Override
+    public CardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.card, parent, false);
+        return new CardHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cards.size();
+    }
+
+    @Override
+    public void onBindViewHolder(CardHolder holder, int position) {
+        Card currentCard = cards.get(position);
+        holder.cardTitle.setText(currentCard.getTitle());
+        holder.cardPrice.setText(currentCard.getPrice().toString());
+        holder.cardQuantity.setText(currentCard.getQuantity());
+
+    }
+
+    //Inner CardHolder class
+    public class CardHolder extends RecyclerView.ViewHolder {
+        public TextView cardTitle;
+        public TextView cardPrice;
+        public TextView cardQuantity;
+
+        //CardHolder constructor
+        public CardHolder(View cardView) {
+            super(cardView);
+
+            cardTitle = (TextView) cardView.findViewById(R.id.productName);
+            cardPrice = (TextView) cardView.findViewById(R.id.productPrice);
+            cardQuantity = (TextView) cardView.findViewById(R.id.productQuantity);
+        }
+
+
+    }
 }
