61f910cdedfd8444f7885e8381dc431aa2287e0d
Adam <datriphopper@gmail.com>
2018-09-30 14:14:37 -0500

    Refactored card class

--- app/src/main/java/com/apps/adam/inventory/Card.java
+++ app/src/main/java/com/apps/adam/inventory/Card.java
@@ -4,20 +4,38 @@ package com.apps.adam.inventory;
 
 public class Card  {
 
+    private int ID;
     private String Title;
+    private String Author;
     private Double Price;
     private Integer Quantity;
+    private String SupplierName;
+    private String SupplierPhone;
 
     //Constructor method
-    public Card(String title, Double price, int quantity) {
+    public Card(int id, String title, String author, Double price, int quantity, String supName, String supPhone) {
+        ID = id;
         Title = title;
+        Author = author;
         Price = price;
         Quantity = quantity;
+        SupplierName = supName;
+        SupplierPhone = supPhone;
     }
 
     //Getter methods to access private variables
+
+
+    public int getID() {
+        return ID;
+    }
+
     public String getTitle() {
         return Title;
+    }
+
+    public String getAuthor() {
+        return Author;
     }
 
     public Double getPrice() {
@@ -26,6 +44,14 @@ public class Card  {
 
     public int getQuantity() {
         return Quantity;
+    }
+
+    public String getSupplierName() {
+        return SupplierName;
+    }
+
+    public String getSupplierPhone() {
+        return SupplierPhone;
     }
 
 
--- app/src/main/java/com/apps/adam/inventory/MainActivity.java
+++ app/src/main/java/com/apps/adam/inventory/MainActivity.java
@@ -1,9 +1,17 @@
 package com.apps.adam.inventory;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.apps.adam.inventory.data.BookContract;
+import com.apps.adam.inventory.data.BookContract.BookEntry;
+
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String[] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMN_PRODUCT_NAME,
+                BookEntry.COLUMN_AUTHOR_NAME,
+                BookEntry.COLUMN_PRICE,
+                BookEntry.COLUMN_QUANTITY,
+                BookEntry.COLUMN_SUPPLIER_NAME,
+                BookEntry.COLUMN_SUPPLIER_PHONE};
+        return new CursorLoader(this, BookEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        //TODO complete this method
+    }
+
+    public void onLoaderReset() {
+        //TODO complete and fix this method
+    }
+
 }
